666ebfe209f1f0ff47505fdd583860fd5cf41204
Eleazar Resendez <e.resendez@outlook.com>
2022-11-25 07:01:45 -0600

    Added Inertia PHP Tests

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,35 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Illuminate\Support\Str;
+use Inertia\Inertia;
+
+// use Illuminate\Support\Str;
+// Route::get('/', function () {
+//     $readme = file_get_contents(public_path('README.md'));
+//     $content = Str::markdown($readme);
+
+//     return view('welcome', compact('content'));
+// });
 
 Route::get('/', function () {
-    $readme = file_get_contents(public_path('README.md'));
-    $content = Str::markdown($readme);
-
-    return view('welcome', compact('content'));
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__ . '/auth.php';
